e5181449eaeaa9bc9c369ea7416b485c2f9cb823
Shawn Dai <daixuan1996@gmail.com>
2017-04-21 02:11:54 +0800

    调整对 PixelArray 的封装

--- src/PixelArray.ts
+++ src/PixelArray.ts
@@ -1,20 +1,38 @@
+import { isNumber } from './Utils';
+
 /**
  * 对 ImageData 的像素数组进行封装
  *
  */
 class PixelArray {
   private pixelArray: Uint8ClampedArray;
+  public width: number;
+  public height: number;
 
   /**
    * ImageData.data
-   * @param {Uint8ClampedArray} array
    */
-  constructor(array : Uint8ClampedArray) {
-    this.pixelArray = array;
+  constructor(array: ImageData, width: number, height?: number);
+  constructor(width: number, height: number);
+  constructor(x?: any, y?: any, z?: any) {
+    if (isNumber(x) && isNumber(y)) {
+      this.pixelArray = new Uint8ClampedArray(x * y * 4);
+      this.width = x;
+      this.height = y;
+    } else if (x && Object.prototype.toString.call(x) === '[object Uint8ClampedArray]' && isNumber(y)) {
+      this.pixelArray = x.data;
+      this.width = y;
+      this.height = isNumber(z) ? z : this.pixelArray.length / (4 * y);
+    } else {
+      throw new Error('PixelArray constructor - 错误的参数类型');
+    }
   }
 
   getPixel(row: number, col: number): number[] {
     let start = row * col * 4;
+    if (start < 0 || start >= this.pixelArray.length) {
+      throw new RangeError('PixelArray getPixel - 参数超过范围');
+    }
     return [
       this.pixelArray[start],
       this.pixelArray[start + 1],
@@ -26,14 +44,25 @@ class PixelArray {
   setPixel(row: number, col: number, pixel: number[]): void {
     let start = row * col * 4;
 
+    if (start < 0 || start >= this.pixelArray.length) {
+      throw new RangeError('PixelArray getPixel - 参数超过范围');
+    }
+
     if (pixel.length != 4) {
-      throw Error('PixelArray setPixel - 错误的参数值');
+      throw new Error('PixelArray setPixel - 错误的参数值');
     }
 
     this.pixelArray[start] = pixel[0];
     this.pixelArray[start + 1] = pixel[1];
     this.pixelArray[start + 2] = pixel[0];
     this.pixelArray[start + 3] = pixel[0];
+  }
+
+  /**
+   * 生成 ImageData
+   */
+  generatImageData() {
+    return new ImageData(this.pixelArray, this.width, this.height);
   }
 }
 
---
+++ src/Utils.ts
@@ -0,0 +1,7 @@
+export function isUndefined(x): boolean {
+  return typeof x === 'undefined';
+}
+
+export function isNumber(x: any): boolean {
+  return typeof x === 'number';
+}
